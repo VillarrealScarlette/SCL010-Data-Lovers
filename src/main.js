0c60272b3c2dba2445cd301c7a404ef94693da2a
dacoquima <danimalistica@gmail.com>
2019-07-05 09:55:25 -0400

    Cambios en JS

--- src/main.js
+++ src/main.js
@@ -3,22 +3,53 @@ for (let i = 0; i < pokemones.length; i++) {
     console.log(pokemones[i].num);
     console.log(pokemones[i].name);
     console.log(pokemones[i].img);
-    //Creando 'div' que será la tarjeta container
+    //Creando 'div' que será la tarjeta container 
     let pokeCards = document.createElement("div");
-    pokeCards.id = pokemones[i].name;
     pokeCards.className = "pokeCards";
     //Creando nombre del pokemon
     let pokeName = document.createElement("h3");
     pokeName.textContent = pokemones[i].name;
-
+    //Creando imagen del pokemon
+    let image = document.createElement("img");
+    image.src = pokemones[i].img;
+    image.id = "pokeImagen";
+    //Creando número del pokemon
+    let pokeNumber = document.createElement("h4");
+    pokeNumber.textContent = "#"+ pokemones[i].num;
+    //Creando tipo de pokemon -> primer tipo
+    let pokeType = document.createElement("p");
+    pokeType.textContent = pokemones[i].type[0];
+    //Creando tipo de pokemon -> segundo tipo
+    pokeType2 = document.createElement("p");
+    pokeType2.textContent = pokemones[i].type[1];
+    //Dando valor a variables
+    pokeCards.appendChild(pokeName);
+    pokeCards.appendChild(pokeNumber);
+    pokeCards.appendChild(image);
+    pokeCards.appendChild(pokeType);
+    pokeCards.appendChild(pokeType2);
     //Mostrar tarjeta en HTLM -> section
-    document.getElementById("container_pokemones").appendChild(pokeCards).innerHTML;
+    document.getElementById("container_pokemones").appendChild(pokeCards).innerHTML; 
+
+/* let openWindow = document.getElementById("pokeImagen");
+let overlay = document.createElement("div");
+overlay.className = "overlay";
+let popup = document.createElement("div");
+popup.className = "popup";
+
+openWindow.addEventListener('click', function() {
+overlay.classList.add('active');
+});
 }
 
-/*let createWindow =
-let window = document.createElement("div id="ventana");
-pokemon.innerHTML = "createElement";
-window.addEventListener('click', openWindow);
-function openWindow(){
+//closeWindow = document.getElementById("cerrarPopUp")
+//cerrarPopUp.id;
 
-}*/
+//Valor 
+const selectType = document.getElementById("type");
+selectType.addEventListener("change", () => {
+    //Condicion = variable
+    let selectUserType = selectType.options[selectType.selectedIndex].value;
+    console.log(selectUserType);
+});
+
