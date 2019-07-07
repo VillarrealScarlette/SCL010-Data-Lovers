cf5f06473eb25e3443e6434b0b51f7419c404584
VillarrealScarlette <villarrealscarlette@gmail.com>
2019-07-07 13:25:17 -0400

    funcionando selectores para filtrar por huevo y por tipo

--- src/data.js
+++ src/data.js
@@ -1,10 +1,32 @@
-/* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-//const example = () => {
-  //return 'example';
-//};
-
-//window.example = example;
+const pokeData = () => {
+  const arrPokemones = POKEMON.pokemon;
+  return arrPokemones;
+}
+window.pokeData = pokeData;
+//Filtrar por Huevos
+const filterEggs = (egg) => {
+  arrPokemones = POKEMON.pokemon;
+  let filters = [];
+  for (i = 0; i < pokemones.length; i++) {
+      if (arrPokemones[i].egg == egg) {
+          filters.push(arrPokemones[i]);
+      }
+  }
+return filters;
+}
+window.filterEggs = filterEggs;
+//Filtrat por Tipo
+const filterTypes = (type) => {
+  arrPokemones = POKEMON.pokemon;
+  let filters = [];
+  for (i = 0; i < pokemones.length; i++) {
+      if (arrPokemones[i].type[0] == type) {
+          filters.push(arrPokemones[i]);
+      } else if (arrPokemones[i].type[1] == type) {
+          filters.push(arrPokemones[i]);
+      }
+  }
+return filters;
+}
+window.filterTypes = filterTypes;
+//Ordenar A-Z
