5822a6e2a1db80c56c712f4d385bf956b0471e8d
VillarrealScarlette <villarrealscarlette@gmail.com>
2019-07-09 17:11:27 -0400

    aplicando test typeof a las funciones de data.js, todos pasan

--- test/data.spec.js
+++ test/data.spec.js
@@ -5,19 +5,40 @@ require('../src/data');
 require('./data.spec.js');
 
 
-describe(window.showByEgg, () => {
+describe(window.filterEggs, () => {
   
-  it('debería ser una función', () => {
-    assert.equal(typeof showByEgg, 'function');
+  it("debería ser una función", () => {
+    assert.equal(typeof filterEggs, "function");
   });
+}); 
+describe(window.filterTypes, () => {  
 
-  it('debería ser una función', () => {
-    assert.equal(typeof showByType, 'function');
+  it("debería ser una función", () => {
+    assert.equal(typeof filterTypes, "function");
   });
+});
+describe(window.ascendingNum, () => {  
 
-  it('debería ser una función', () => {
-    assert.equal(typeof filter, 'function');
+  it("debería ser una función", () => {
+    assert.equal(typeof ascendingNum, "function");
   });
-})
+});
+describe(window.descendingNum, () => { 
+
+  it("debería ser una función", () => {
+    assert.equal(typeof descendingNum, "function");
+  });
+});
+describe(window.descendingName, () => { 
+
+  it("debería ser una función", () => {
+    assert.equal(typeof descendingName, "function");
+  });
+});
+describe(window.ascendingName, () => {  
+  it("debería ser una función", () => {
+    assert.equal(typeof ascendingName, "function");
+  });
+});
 
 //assert.deepEqual
